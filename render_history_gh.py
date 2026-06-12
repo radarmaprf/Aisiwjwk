4ed3accc2327c11de1208ff905dcaf8ed3328031
radarmaprf <y697b8yzz2@privaterelay.appleid.com>
2026-06-12 13:50:59 +0500

    Update render_history_gh.py

--- render_history_gh.py
+++ render_history_gh.py
@@ -13,8 +13,8 @@ from matplotlib.transforms import Bbox
 # ========== НАСТРОЙКИ ==========
 ALERTS_FILE = "alerts.json"
 OUTPUT_VIDEO = "alerts_timelapse.mp4"
-FPS = 7
-MAX_COMMITS = 120
+FPS = 20
+MAX_COMMITS = 300
 COLORS = {
     "clear": "#6c757d",
     "droneDanger": "#ffc107",
